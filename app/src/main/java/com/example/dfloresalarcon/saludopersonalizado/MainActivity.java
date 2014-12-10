a7668ae0a37489ad8b51619f88e5125b202c8eae
dfloresalarcon <dfloresalarcon@danielcastelao.org>
2014-12-10 11:43:39 +0100

    Commit Funcionando Activity

--- app/src/main/java/com/example/dfloresalarcon/saludopersonalizado/MainActivity.java
+++ app/src/main/java/com/example/dfloresalarcon/saludopersonalizado/MainActivity.java
@@ -4,14 +4,44 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends Activity {
 
+    String saludo="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnHola = (Button)findViewById(R.id.btnHola);
+        final EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        final TextView salida = (TextView)findViewById(R.id.salida);
+
+
+
+        btnHola.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioGroup radio = (RadioGroup)findViewById(R.id.agruparRadios);
+                if (R.id.rbtnSr == radio.getCheckedRadioButtonId()){
+                    saludo+=" Sr. "+txtNombre.getText().toString();
+                }
+                else{
+                    saludo+=" Sra. "+txtNombre.getText().toString();
+                }
+
+                salida.setText(btnHola.getText()+saludo);
+            }
+        });
+
     }
 
 
@@ -19,6 +49,8 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+
         return true;
     }
 
